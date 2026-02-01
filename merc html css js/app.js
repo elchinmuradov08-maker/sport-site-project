9c1288ddb8de705a7225a58288e2816b5e9d58be
elchinmuradov08-maker <elchinmuradov08@gmail.com>
2026-02-01 14:37:32 +0400

    Add files via upload

---
+++ merc html css js/app.js
@@ -0,0 +1,73 @@
+// Elementi ID ilə çağırırıq
+const khabibBtn = document.getElementById("khabibBtn");
+const haciBtn = document.getElementById("haciBtn");
+const hockeyBtn = document.getElementById("hockeyBtn");
+const topBtn = document.getElementById("topBtn");
+const mmaBtn = document.getElementById("mmaBtn");
+const hockBtn = document.getElementById("hockBtn");
+const ftblBtn = document.getElementById("ftblBtn");
+const bsktBtn = document.getElementById("bsktBtn");
+const vlybBtn = document.getElementById("vlybBtn");
+const misliBtn = document.getElementById("misliBtn");
+const azerBtn = document.getElementById("azerBtn");
+const turboBtn = document.getElementById("turboBtn");
+// Khabib vs Conor
+khabibBtn.addEventListener("click", () => {
+  const url = "https://www.youtube.com/watch?v=JuBBIJ7adjM"; 
+  window.open(url, "_blank", "noopener"); 
+});
+// Haci vs Rashidov
+haciBtn.addEventListener("click", () => {
+  const url = "https://www.youtube.com/watch?v=IOe19L-5-rE"; 
+  window.open(url, "_blank", "noopener");
+}); 
+//canada vs russia
+hockeyBtn.addEventListener("click", () => {
+  const url = "https://www.youtube.com/watch?v=olpovYyqaog"; 
+  window.open(url, "_blank", "noopener");
+});
+//bu gunki oyunlar
+topBtn.addEventListener("click", () => {
+  const url = "http://m.flashscore.com.tr/"; 
+  window.open(url, "_blank", "noopener");
+});
+//bu gunki doyusler
+mmaBtn.addEventListener("click", () => {
+  const url = "https://www.flashscore.com.tr/mma/"; 
+  window.open(url, "_blank", "noopener");
+});
+//bu gunki hockey maclari
+hockBtn.addEventListener("click", () => {
+  const url = "https://www.nesine.com/iddaa/canli-skor/buz-hokeyi"; 
+  window.open(url, "_blank", "noopener");
+});
+//qarabag vs ajax
+ftblBtn.addEventListener("click", () => {
+  const url = "https://www.youtube.com/watch?v=OFHOcWI4k5A"; 
+  window.open(url, "_blank", "noopener");
+});
+//boston vs toronto 
+bsktBtn.addEventListener("click", () => {
+  const url = "https://www.youtube.com/watch?v=0f-89gT639w"; 
+  window.open(url, "_blank", "noopener");
+});
+//voleybol maci
+vlybBtn.addEventListener("click", () => {
+  const url = "https://www.youtube.com/watch?v=jOzj_patgnQ"; 
+  window.open(url, "_blank", "noopener");
+});
+//misli 
+misliBtn.addEventListener("click", () => {
+  const url = "https://www.misli.az/"; 
+  window.open(url, "_blank", "noopener");
+});
+//azercell
+azerBtn.addEventListener("click", () => {
+  const url = "https://www.azercell.com/"; 
+  window.open(url, "_blank", "noopener");
+});
+//turboaz
+turboBtn.addEventListener("click", () => {
+  const url = "https://turbo.az/"; 
+  window.open(url, "_blank", "noopener");
+});
